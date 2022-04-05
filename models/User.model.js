d6dd223a23b68b4481d9c024e7ea6d4f62dce11e
Patricia <patricia.benites.cava@gmail.com>
2022-04-05 20:36:48 +0200

    initial setup and user model

---
+++ models/User.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ routes/auth.routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const passwordCorrect = await bcrypt.compare(password, user.password);
+      if (passwordCorrect) {
+        const payload = {
+          user,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "6h",
+        });
+        res.status(200).json({
+          user,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: "Email or password are incorrect" });
+      }
+    } else {
+      res.status(401).json({ message: "Email or password are incorrect" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
