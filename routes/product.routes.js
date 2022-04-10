ed7066c410cf2c55259f4f8d4d7bbd2e8cc8d066
Patricia <patricia.benites.cava@gmail.com>
2022-04-10 15:57:22 +0200

    add get products route

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -40,4 +40,36 @@ router.delete("/:id", authenticate, async (req, res) => {
   }
 });
 
+// / Get Product
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get all products ->
+router.get("/find", async (req, res) => {
+  try {
+    let products;
+
+    if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
