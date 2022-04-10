8969efc80f0dea34f570fe4c1ad7fc99ac285ba9
Patricia <patricia.benites.cava@gmail.com>
2022-04-10 15:23:07 +0200

    add create and update products routes

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,6 +1,32 @@
 const express = require("express");
+const { authenticate } = require("../middlewares/jwt.middleware");
 const Product = require("../models/Product.model");
 
 const router = express.Router();
+
+// Create product -> Admin only
+
+router.post("/", authenticate, async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(200).json(newProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//  Update product -> Admin only
+router.put("/:id", authenticate, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
