0994cd37128f76957ca6a7a33428f36d56877784
Patricia <patricia.benites.cava@gmail.com>
2022-04-10 14:40:41 +0200

    add delete user function

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -67,4 +67,62 @@ router.put("/:id", authenticate, async (req, res) => {
   }
 });
 
+router.delete("/:id", authenticate, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get User -> only for Admin
+
+router.get("/find/:id", authenticate, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...details } = user._doc;
+    res.status(200).json(details);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get all users -> only for Admin
+router.get("/find", authenticate, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//  Get user stats -. only for Admin
+
+router.get("/stats", authenticate, async (req, res) => {
+  const date = new Date();
+  const lastyear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastyear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
