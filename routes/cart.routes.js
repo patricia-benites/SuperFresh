042677aebae20bf614a8af78b67cfb464e92a898
Patricia <patricia.benites.cava@gmail.com>
2022-04-10 16:20:47 +0200

    add create cart route

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,6 +1,77 @@
 const express = require("express");
 const Cart = require("../models/Cart.model");
+const { authenticate } = require("../middlewares/jwt.middleware");
 
 const router = express.Router();
+
+// Create cart
+
+router.post("/", authenticate, async (req, res) => {
+    try {
+      const newCart = await Cart.create(req.body);
+      res.status(200).json(newCart);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+//   //  Update product -> Admin only
+//   router.put("/:id", authenticate, async (req, res) => {
+//     try {
+//       const updatedProduct = await Product.findByIdAndUpdate(
+//         req.params.id,
+//         { $set: req.body },
+//         { new: true }
+//       );
+//       res.status(200).json(updatedProduct);
+//     } catch (error) {
+//       res.status(500).json(error);
+//     }
+//   });
+  
+//   //  Delete product -> Admin only
+  
+//   router.delete("/:id", authenticate, async (req, res) => {
+//     try {
+//       await Product.findByIdAndDelete(req.params.id);
+//       res.status(200).json("Product has been deleted");
+//     } catch (error) {
+//       res.status(500).json(error);
+//     }
+//   });
+  
+//   // / Get Product
+  
+//   router.get("/find/:id", async (req, res) => {
+//     try {
+//       const product = await Product.findById(req.params.id);
+//       res.status(200).json(product);
+//     } catch (error) {
+//       res.status(500).json(error);
+//     }
+//   });
+  
+//   // Get all products ->
+//   router.get("/find", async (req, res) => {
+//     const qCategory = req.query.category;
+  
+//     let products;
+//     try {
+//       if (qCategory) {
+//         products = await Product.find({
+//           categories: {
+//             $in: [qCategory],
+//           },
+//         });
+//       } else {
+//         products = await Product.find();
+//       }
+  
+//       res.status(200).json(products);
+//     } catch (error) {
+//       res.status(500).json(error);
+//     }
+//   });
+
 
 module.exports = router;
